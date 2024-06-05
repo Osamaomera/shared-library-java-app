9d26fc4f063f61457423b6ac8ac9ba1aedfc6276
Osamaomera <osama.omera68@gmail.com>
2024-06-05 22:07:37 +0300

    oc

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 // OpenShiftCredentialsID 'Service Account Token'
-def call(String OpenshiftcredintialsID, String imageName, String openshiftServer, String openshiftProject) {
+def call(String OpenshiftcredintialsID, String imageName) {
     
     // Update deployment configuration with new Docker Hub image tag
     sh "sed -i 's|image:.*|image: ${imageName}|g' java-deployment-service.yml"
 
     withKubeCredentials(kubectlCredentials: [[caCertificate: '', clusterName: '', contextName: '', credentialsId: 'token', namespace: 'osamaayman', serverUrl: 'https://api.ocp-training.ivolve-test.com:6443']]) {
-        sh "oc apply -f java-deployment-service.yml"
+        sh "oc apply -f java-deployment-service.yml -n osamaayman"
     }
 }
