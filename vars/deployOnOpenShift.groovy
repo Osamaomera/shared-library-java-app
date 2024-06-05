e3b78b5c2295eea1a602c334f53bfe95f8899e6d
Osamaomera <osama.omera68@gmail.com>
2024-06-05 21:59:18 +0300

    oc

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -3,27 +3,9 @@
 def call(String OpenshiftcredintialsID, String imageName, String openshiftServer, String openshiftProject) {
     
     // Update deployment configuration with new Docker Hub image tag
-    sh "sed -i 's|image:.*|image: ${imageName}|g' java-deployment.yml"
+    sh "sed -i 's|image:.*|image: ${imageName}|g' java-deployment-service.yml"
 
-        // withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'openshift', namespace: 'osamaayman', restrictKubeConfigAccess: false, serverUrl: 'https://api.ocp-training.ivolve-test.com:6443') {
-        withKubeCredentials(kubectlCredentials: [[caCertificate: '', clusterName: '', contextName: '', credentialsId: 'token', namespace: 'osamaayman', serverUrl: 'https://api.ocp-training.ivolve-test.com:6443']]) {
+    withKubeCredentials(kubectlCredentials: [[caCertificate: '', clusterName: '', contextName: '', credentialsId: 'token', namespace: 'osamaayman', serverUrl: 'https://api.ocp-training.ivolve-test.com:6443']]) {
         sh "oc apply -f java-deployment-service.yml"
     }
 }
-
-
-
-
-// #!/usr/bin/env groovy
-
-// // KubernetesCredentialsID 'KubeConfig file'
-// def call(String k8sCredentialsID, String imageName) {
-    
-//     // Update deployment.yaml with new Docker Hub image
-//     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' java-deployment.yml"
-
-//     // login to k8s Cluster via KubeConfig file
-//     withCredentials([file(credentialsId: "${k8sCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
-//         sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f myapp-deployment.yml"
-//     }
-// }
