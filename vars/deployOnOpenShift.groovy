8ff8024c8feec57f5bc9f8fb3a45336621cd04eb
Osamaomera <osama.omera68@gmail.com>
2024-06-05 21:49:51 +0300

    oc

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -6,30 +6,7 @@ def call(String OpenshiftcredintialsID, String imageName, String openshiftServer
     sh "sed -i 's|image:.*|image: ${imageName}|g' java-deployment.yml"
 
         // withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'openshift', namespace: 'osamaayman', restrictKubeConfigAccess: false, serverUrl: 'https://api.ocp-training.ivolve-test.com:6443') {
-        withKubeCredentials(kubectlCredentials: [[caCertificate: '''apiVersion: v1
-clusters:
-- cluster:
-    insecure-skip-tls-verify: true
-    server: https://api.ocp-training.ivolve-test.com:6443
-  name: api-ocp-training-ivolve-test-com:6443
-contexts:
-- context:
-    cluster: api-ocp-training-ivolve-test-com:6443
-    namespace: default
-    user: osamaayman/api-ocp-training-ivolve-test-com:6443
-  name: default/api-ocp-training-ivolve-test-com:6443/osamaayman
-- context:
-    cluster: api-ocp-training-ivolve-test-com:6443
-    namespace: osamaayman
-    user: osamaayman/api-ocp-training-ivolve-test-com:6443
-  name: osamaayman/api-ocp-training-ivolve-test-com:6443/osamaayman
-current-context: osamaayman/api-ocp-training-ivolve-test-com:6443/osamaayman
-kind: Config
-preferences: {}
-users:
-- name: osamaayman/api-ocp-training-ivolve-test-com:6443
-  user:
-    token: sha256~wzsgwiEvmr3CejJNqKZBZZRklliA3MOuE8IGAM0FbJk''', clusterName: '', contextName: '', credentialsId: 'token', namespace: 'osamayman', serverUrl: 'https://api.ocp-training.ivolve-test.com:6443']]) {
+        withKubeCredentials(kubectlCredentials: [[caCertificate: '', clusterName: '', contextName: '', credentialsId: 'token', namespace: 'osamaayman', serverUrl: 'https://api.ocp-training.ivolve-test.com:6443']]) {
         sh """
             oc apply -f java-deployment-service.yml
         """
