f1144cac9bc0f734c2ddb83e315edb3b075905bc
Osamaomera <osama.omera68@gmail.com>
2024-06-05 15:34:46 +0300

    oc

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -6,6 +6,7 @@ def call(String SERVICE_ACCOUNT_TOKEN, String imageName, String openshiftServer,
     sh "sed -i 's|image:.*|image: ${imageName}|g' java-deployment.yml"
 
         sh """
+            oc login ${openshiftServer} --token=${SERVICE_ACCOUNT_TOKEN}
             oc project ${openshiftProject}  // Specify your project name
             oc apply -f java-deployment.yml
             oc apply -f java-service.yml 
